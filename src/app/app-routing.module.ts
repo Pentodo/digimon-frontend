556e1a4fda4a77ebee6cbf5daa02669337c607ae
Pentodo <matheus.a.v.b@gmail.com>
2022-12-08 14:34:46 -0300

    setted home page and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DigimonComponent } from './digimon/digimon.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DigimonComponent,
+    title: 'Digimons',
+  },
+  {
+    path: 'levels',
+    component: DigimonComponent,
+    title: 'Digievoluções',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
