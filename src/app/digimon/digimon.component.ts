9e2cd455d4a0c1d073cb77af8f992d04f7f69f32
Pentodo <matheus.a.v.b@gmail.com>
2022-12-09 00:06:32 -0300

    finished sort filters support

--- src/app/digimon/digimon.component.ts
+++ src/app/digimon/digimon.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+
 import { Digimon } from './digimon.interface';
 import { DigimonService } from './digimon.service';
 
@@ -9,9 +10,10 @@ import { DigimonService } from './digimon.service';
 })
 export class DigimonComponent {
   digimons: Digimon[] = [];
-  filteredDigimons?: Digimon[];
+  filteredDigimons: Digimon[] | undefined;
 
   filter: string = '';
+  order: string = 'name';
 
   constructor(private readonly digimonService: DigimonService) {}
 
@@ -20,18 +22,45 @@ export class DigimonComponent {
   }
 
   getDigimons(): void {
-    this.digimonService
-      .getDigimons()
-      .subscribe((digimons) => (this.digimons = digimons));
+    this.digimonService.getDigimons().subscribe((digimons) => {
+      this.digimons = digimons;
+      this.sortDigimons();
+    });
   }
 
   filterDigimons(): void {
     this.filteredDigimons = this.filter
-      ? this.digimons.filter((digimon) => {
-          return (digimon.name + digimon.level)
+      ? this.digimons.filter((digimon) =>
+          `${digimon.name} ${digimon.level}`
             .toLowerCase()
-            .includes(this.filter.toLowerCase());
-        })
+            .includes(this.filter.toLowerCase())
+        )
       : undefined;
+  }
+
+  sortDigimons(): void {
+    const levels: any = {
+      Fresh: 0,
+      'In Training': 1,
+      Rookie: 2,
+      Champion: 3,
+      Ultimate: 4,
+      Mega: 5,
+      Armor: 6,
+    };
+
+    function sortByName(a: Digimon, b: Digimon) {
+      return a.name > b.name ? 1 : -1;
+    }
+    function sortByLevel(a: Digimon, b: Digimon) {
+      return levels[a.level] - levels[b.level];
+    }
+
+    const sortFunction = this.order === 'name' ? sortByName : sortByLevel;
+
+    this.digimons.sort(sortFunction);
+    if (this.filteredDigimons) {
+      this.filteredDigimons.sort(sortFunction);
+    }
   }
 }
